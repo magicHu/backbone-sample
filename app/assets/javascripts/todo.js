0dcd7f95b1f1717b64c03c49aa7fd71ccb261fdc
magicHu <huronghai2008@gmail.com>
2013-03-26 15:09:52 +0800

    todo list tools

---
+++ app/assets/javascripts/todo.js
@@ -0,0 +1,93 @@
+$(function() {
+  var Todo = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        title: "empty todo...",
+        order: Todos.nextOrder(),
+        done: false
+      }
+    }, 
+
+    toggle: function() {
+      this.save({done: !this.get("done")});
+    }
+  });
+
+
+  var TodoList = Backbone.Collection.extend({
+    model: Todo,
+    
+    localStorage: new Backbone.LocalStorage("todos-backbone"),
+    
+    done: function() {
+      return this.where({done: true});
+    }, 
+
+    remaining: function() {
+      return this.without.apply(this, this.done());
+    },
+
+    nextOrder: function() {
+      if(!this.length) return 1;
+
+      return this.last().get('order') + 1;
+    }, 
+
+    comparator: 'order'
+  });
+
+  var Todos = new TodoList;
+
+  var TodoView = Backbone.View.extend({
+    tagName: "li",
+
+    template: _.template($('#item-template').html()),
+
+    events: {
+      "click .toggle": "toggleDone",
+      "dblclick .view": "edit", 
+      "click a.destroy": "clear",
+      "keypress .edit": "updateOnEnter",
+      "blur .edit": "close"
+    },
+
+    initialize: function() {
+      this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'destroy', this.remove);
+    },
+
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+      this.$el.toggleClass('done', this.model.get('done'));
+      this.input = this.$('.edit');
+      return this;
+    },
+
+    toggleDone: function() {
+      this.model.toggle();
+    }, 
+
+    edit: function() {
+      this.$el.addClass('editing');
+      this.input.focus();
+    },
+
+    close: function() {
+      var value = this.input.val();
+      if (!value) {
+        this.clear();
+      } else {
+        this.model.save({title: value});
+        this.$el.removeClass('editing');
+      }
+    }, 
+
+    updateOnEnter: function(e) {
+      if (e.keycode == 13) this.close();
+    }, 
+
+    clear: function() {
+      this.model.destroy();
+    }
+  });
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneSample::Application.config.secret_token = '09c99fd70f2be11defc9811427f6caf835c9b9296af8a9867b57acd4deb551273326a64a33ad24ad12cf5d333b1d98b3b5216bad32b6245ed2460a05d6a2e5f1'
